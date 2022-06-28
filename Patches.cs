c13c4e1a3e6eaf71010e362b363e79eea1dba595
AzumattDev <azumattgaming@gmail.com>
2022-06-28 00:02:45 -0400

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HammerPins;
+
+public class DisplayPinsOnMap
+{
+    private static readonly Dictionary<ZDO, Minimap.PinData> customPins = new();
+    private static readonly Dictionary<int, Sprite> icons = new();
+    private static readonly int hammerHashCode = "Hammer".GetStableHashCode();
+    private static readonly float updateInterval = 5.0f;
+    internal static string[] prefabArray;
+
+    // clear dictionary if the user logs out
+    [HarmonyPatch(typeof(Minimap), nameof(Minimap.OnDestroy))]
+    public static class MinimapOnDestroyPatch
+    {
+        private static void Postfix()
+        {
+            customPins.Clear();
+            icons.Clear();
+            Array.Clear(prefabArray, 0, prefabArray.Length);
+        }
+    }
+
+    [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateMap))]
+    public static class MinimapUpdateMapPatch
+    {
+        private static float timeCounter = updateInterval;
+
+        public static void Postfix(ref Minimap __instance, Player player, float dt, bool takeInput)
+        {
+            timeCounter += dt;
+
+            /*if (timeCounter < updateInterval || !OdinQOLplugin.mapIsEnabled.Value ||
+                !HammerPinsPlugin.displayPins.Value)
+                return;*/
+            if (timeCounter < updateInterval ||
+                HammerPinsPlugin.DisplayPins.Value == HammerPinsPlugin.Toggle.Off)
+                return;
+            if (HammerPinsPlugin.IconsToPin != null && HammerPinsPlugin.IconsToPin.Value.Length > 0)
+            {
+                prefabArray = HammerPinsPlugin.IconsToPin.Value.Trim().Split(',').ToArray();
+            }
+            else
+            {
+                customPins.Clear();
+                icons.Clear();
+                Array.Clear(prefabArray, 0, prefabArray.Length);
+                return;
+            }
+
+            timeCounter -= updateInterval;
+
+            if (icons.Count == 0)
+                FindIcons();
+
+
+            // search zones for ships and carts
+            foreach (List<ZDO> zdoarray in ZDOMan.instance.m_objectsBySector)
+                if (zdoarray != null)
+                    foreach (ZDO zdo in zdoarray)
+                    {
+                        foreach (string prefab in prefabArray)
+                        {
+                            if (CheckPin(__instance, player, zdo, prefab.GetStableHashCode(), ""))
+                                continue;
+                        }
+                    }
+
+            // clear pins for destroyed objects
+            foreach (KeyValuePair<ZDO, Minimap.PinData> pin in customPins.Where(pin => !pin.Key.IsValid()))
+            {
+                __instance.RemovePin(pin.Value);
+                customPins.Remove(pin.Key);
+            }
+        }
+
+        private static bool CheckPin(Minimap __instance, Player player, ZDO zdo, int hashCode, string pinName)
+        {
+            if (zdo.m_prefab != hashCode)
+                return false;
+
+            Minimap.PinData customPin;
+            bool pinWasFound = customPins.TryGetValue(zdo, out customPin);
+
+            // turn off associated pin if player controlled ship is in that position
+            Ship controlledShip = player.GetControlledShip();
+            if (controlledShip && Vector3.Distance(controlledShip.transform.position, zdo.m_position) < 0.01f)
+            {
+                if (pinWasFound)
+                {
+                    __instance.RemovePin(customPin);
+                    customPins.Remove(zdo);
+                }
+
+                return true;
+            }
+
+            if (!pinWasFound)
+            {
+                customPin = __instance.AddPin(zdo.m_position, Minimap.PinType.Death, pinName, false, false);
+
+                Sprite sprite;
+                if (icons.TryGetValue(hashCode, out sprite))
+                    customPin.m_icon = sprite;
+
+                customPin.m_doubleSize = false;
+                customPins.Add(zdo, customPin);
+            }
+            else
+            {
+                customPin.m_pos = zdo.m_position;
+            }
+
+            return true;
+        }
+
+        private static void FindIcons()
+        {
+            GameObject hammer = ObjectDB.instance.m_itemByHash[hammerHashCode];
+            if (!hammer)
+                return;
+            ItemDrop hammerDrop = hammer.GetComponent<ItemDrop>();
+            if (!hammerDrop)
+                return;
+            PieceTable hammerPieceTable = hammerDrop.m_itemData.m_shared.m_buildPieces;
+            foreach (Piece p in hammerPieceTable.m_pieces.Select(piece => piece.GetComponent<Piece>()))
+            {
+                icons.Add(p.name.GetStableHashCode(), p.m_icon);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+
+namespace HammerPins
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class HammerPinsPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "HammerPins";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource HammerPinsLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            DisplayPins = config("1 - General", "Display Pins", Toggle.On, "Show the pins on the map");
+
+            IconsToPin = config("2 - Icons", "Icons to Pin",
+                "guard_stone,VikingShip,Cart,Karve,portal_wood,bed,piece_bed02,Raft",
+                new ConfigDescription("Prefab names of the pins from your hammer you wish to show on the map", null,
+                    new ConfigurationManagerAttributes { CustomDrawer = MyDrawer }));
+
+            IconsToPin.SettingChanged += (_, _) =>
+            {
+                Array.Clear(DisplayPinsOnMap.prefabArray, 0, DisplayPinsOnMap.prefabArray.Length);
+                DisplayPinsOnMap.prefabArray = IconsToPin.Value.Trim().Split(',').ToArray();
+            };
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                HammerPinsLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                HammerPinsLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                HammerPinsLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+        internal static ConfigEntry<Toggle> DisplayPins = null!;
+        internal static ConfigEntry<string>? IconsToPin;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private sealed class ConfigurationManagerAttributes
+        {
+            //public bool? Browsable = false;
+            public Action<ConfigEntryBase> CustomDrawer;
+        }
+
+        static void MyDrawer(ConfigEntryBase entry)
+        {
+            bool wasUpdated = false;
+            GUILayout.BeginVertical();
+            string? prefabs = entry.BoxedValue.ToString();
+            string newItemName = GUILayout.TextArea(prefabs,
+                new GUIStyle(GUI.skin.textArea) { stretchHeight = true });
+            wasUpdated = wasUpdated || !ReferenceEquals(prefabs, newItemName);
+            GUILayout.EndVertical();
+            if (wasUpdated)
+            {
+                entry.BoxedValue = newItemName;
+            }
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+    }
+}
